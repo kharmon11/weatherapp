be52b90ae101670eddd6a22ae61b052946b00684
kharmon11 <kharmon11@gmail.com>
2017-11-21 12:36:41 -0800

    v0.1.3 Fixed current humidity calculation bug

--- src/components/body/overview/current/other/Other.js
+++ src/components/body/overview/current/other/Other.js
@@ -5,10 +5,9 @@ import OtherRow from './otherRow/OtherRow';
 import './Other.css';
 
 function Other(props) {
-    
     return (
         <div id="other_current_data" className="current-wx">
-          <OtherRow col1={"Dewpoint: " + Math.round(props.currently.dewPoint) + String.fromCharCode(176) + "F"} col2={"Humidity: " + Math.round(props.currently.humidity) + "%"}/>
+          <OtherRow col1={"Dewpoint: " + Math.round(props.currently.dewPoint) + String.fromCharCode(176) + "F"} col2={"Humidity: " + Math.round(props.currently.humidity * 100) + "%"}/>
           <OtherRow col1={"Precip: " + props.currently.precipProbability * 100 + "%"} col2={"Rate: " + props.currently.precipIntensity + "in/hr"}/>
           <OtherRow col1={"Speed: " + Math.round(props.currently.windSpeed) + "mph"} col2={"Gust: " + Math.round(props.currently.windGust) + "mph"}/>
           <OtherRow col1={"SLP: " + Math.round(props.currently.pressure) + "mb"} col2={"Visibility: " + props.currently.visibility + "mi"}/>
