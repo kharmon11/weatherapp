d0b43fccea5d02e399612ba5c97ac5bf938863a9
Ken Harmon <kharmon11@gmail.com>
2018-05-26 11:28:32 -0700

    Various edits

--- letsencrypt.py
+++ letsencrypt.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 credentials = {
-    'HIYhFjSe-lsIiE-NXICLyGUJ1t0i49l_qmxNgI9Escw': 'HIYhFjSe-lsIiE-NXICLyGUJ1t0i49l_qmxNgI9Escw.ihJoBpXQunhCLoi9xariZdOQME-wxdEkUFK1EXckUlY'
+    'lobq2J4LfHW9QOvLMjSZvyTfMQDS5YLvtu9Cwz_FdQs': 'lobq2J4LfHW9QOvLMjSZvyTfMQDS5YLvtu9Cwz_FdQs.9YZjj1DdVYZYTDE8QuB3FJERyynkmZz5tSVwicFt2TI'
 }
+
+# credentials = {
+#     'HIYhFjSe-lsIiE-NXICLyGUJ1t0i49l_qmxNgI9Escw': 'HIYhFjSe-lsIiE-NXICLyGUJ1t0i49l_qmxNgI9Escw.ihJoBpXQunhCLoi9xariZdOQME-wxdEkUFK1EXckUlY'
+# }
 
 
 @app.route('/.well-known/acme-challenge/<challenge>')
