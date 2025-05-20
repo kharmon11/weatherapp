95cf75935e7f9d67bb1b74dfa3860f97d3f4171a
Ken Harmon <kharmon11@gmail.com>
2025-05-20 16:25:42 -0700

    More environment configuration.

--- server/app/main.py
+++ server/app/main.py
@@ -1,14 +1,23 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .api.openweathermap import router as openweathermap_router
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:5173",
-    "localhost:5173"
-]
+# origins = [
+#     "http://localhost:5173",
+#     "localhost:5173",
+#     "https://weather.kenharmon.net"
+# ]
 
+if os.getenv("ENV") == "production":
+    origins = os.getenv("ALLOWED_ORIGINS", "http://localhost:5173").split(",")
+else:
+    origins = ["http://localhost:5173", "localhost:5173"]
+
+print(origins)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
