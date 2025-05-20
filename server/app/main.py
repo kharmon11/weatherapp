c2bcf74e95d29fd108a70c1108e975cc248378fe
Ken Harmon <kharmon11@gmail.com>
2025-05-20 16:47:06 -0700

    Preparations for deployment

--- server/app/main.py
+++ server/app/main.py
@@ -2,22 +2,16 @@ import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from .api.openweathermap import router as openweathermap_router
 
 app = FastAPI()
-
-# origins = [
-#     "http://localhost:5173",
-#     "localhost:5173",
-#     "https://weather.kenharmon.net"
-# ]
-
-if os.getenv("ENV") == "production":
+ENV = os.getenv("ENV")
+if ENV == "production":
     origins = os.getenv("ALLOWED_ORIGINS", "http://localhost:5173").split(",")
 else:
     origins = ["http://localhost:5173", "localhost:5173"]
 
-print(origins)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -26,9 +20,13 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+if ENV == "production":
+    frontend_path = os.path.join(os.path.dirname(__file__), "../client-dist")
+    app.mount("/", StaticFiles(directory=frontend_path, html=True), name="static")
+
 @app.get("/")
 async def root():
-    return "Hello World"
+    return {"message": "Backend Running & Serving Frontend"}
 
 # /api/openweathermap endpoint
 app.include_router(openweathermap_router, prefix="/api")
