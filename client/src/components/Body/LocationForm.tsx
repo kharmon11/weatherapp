b0454f467e011a6d221fa1a8951c07ff0cfbe724
Ken Harmon <kharmon11@gmail.com>
2025-06-17 12:47:18 -0700

    fix: Updated responsiveness of location form

--- client/src/components/Body/LocationForm.tsx
+++ client/src/components/Body/LocationForm.tsx
@@ -22,18 +22,24 @@ export default function LocationForm({locationError, handleSubmit, handleMyLocat
             {locationError && (
                 <div className={"geolocation-error"}>{locationError}</div>
             )}
-            <div className={"location-form-header"}>Enter Location</div>
-            <input
-                id="location-input"
-                className={"location-input"}
-                type="text"
-                placeholder={"Enter Location"}
-                value={location}
-                onChange={e => setLocation(e.target.value)}
-            />
-            <button id={"location-submit"} type={"submit"}>Submit</button>
-            <button id={"my-location-btn"} type={"button"} onClick={handleMyLocation}><MdMyLocation/>My Location
-            </button>
+            <div className={"location-form-controls"}>
+                <div className={"location-input-wrapper"}>
+                    <div className={"location-form-header"}>Enter Location</div>
+                    <input
+                        id="location-input"
+                        className={"location-input"}
+                        type="text"
+                        placeholder={"city, state, zipcode, etc."}
+                        value={location}
+                        onChange={e => setLocation(e.target.value)}
+                    />
+                </div>
+                <div className={"location-buttons-wrapper"}>
+                    <button id={"location-submit"} type={"submit"}>Submit</button>
+                    <button id={"my-location-btn"} type={"button"} onClick={handleMyLocation}><MdMyLocation/>My Location
+                    </button>
+                </div>
+            </div>
         </form>
     )
 }
