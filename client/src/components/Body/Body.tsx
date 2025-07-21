56ef80e69b61f652ac81b789e938a9f004af4729
Ken Harmon <kharmon11@gmail.com>
2025-07-21 16:45:12 -0700

    fix: WindVane rotation glitch repaired

--- client/src/components/Body/Body.tsx
+++ client/src/components/Body/Body.tsx
@@ -8,62 +8,62 @@ import Current from "./Current.tsx";
 import WeekForecast from "./WeekForecast/WeekForecast.tsx"
 
 export default function Body() {
-    const {
-        weather,
-        isLoading,
-        locationError,
-        googleMapError,
-        fetchWeather,
-        fetchWeatherByGeolocation,
-        fetchWeatherByMapClick
-    } = useWeather();
+  const {
+    weather,
+    isLoading,
+    locationError,
+    googleMapError,
+    fetchWeather,
+    fetchWeatherByGeolocation,
+    fetchWeatherByMapClick
+  } = useWeather();
 
-    // Fetch weather data when #location-submit button is clicked
-    const handleSubmit = async (locationInput: string) => {
-        // e.preventDefault()
-        await fetchWeather(locationInput)
-    }
+  // Fetch weather data when #location-submit button is clicked
+  const handleSubmit = async (locationInput: string) => {
+    // e.preventDefault()
+    await fetchWeather(locationInput)
+  }
 
-    // Handle when #my-location-btn is clicked
-    const handleMyLocation = () => {
-        fetchWeatherByGeolocation()
-    }
+  // Handle when #my-location-btn is clicked
+  const handleMyLocation = () => {
+    fetchWeatherByGeolocation()
+  }
 
-    // Handle map click - just pass the event to the hook function
-    const handleMapClick = async (event: MapMouseEvent) => {
-        await fetchWeatherByMapClick(event)
-    }
+  // Handle map click - just pass the event to the hook function
+  const handleMapClick = async (event: MapMouseEvent) => {
+    await fetchWeatherByMapClick(event)
+  }
 
-    return (
-        <main className="main">
-            <div className={"weather-header"}>Weather</div>
-            <LocationForm
-                locationError={locationError}
-                handleSubmit={handleSubmit}
-                handleMyLocation={handleMyLocation}
+  return (
+    <main className="main">
+      <div className={"weather-header"}>Weather</div>
+      <LocationForm
+        locationError={locationError}
+        handleSubmit={handleSubmit}
+        handleMyLocation={handleMyLocation}
+      />
+      {isLoading && (
+        <Spinner/>
+      )}
+      {weather && (
+        <div className={"weather-output"}>
+          <div className={"current-wrapper"}>
+            <Current
+              current={weather.data.current}
+              minutely={weather.data.minutely ? weather.data.minutely : []}
+              timezone={weather.data.timezone}
+              location_text={weather.location_text}
+              lat={weather.data.lat}
+              lon={weather.data.lon}
+              lat_string={weather.lat_string}
+              lon_string={weather.lon_string}
+              handleMapClick={handleMapClick}
+              googleMapError={googleMapError}
             />
-            {isLoading && (
-                <Spinner/>
-            )}
-            {weather && (
-                <div className={"weather-output"}>
-                    <div className={"current-wrapper"}>
-                        <Current
-                            current={weather.data.current}
-                            minutely={weather.data.minutely? weather.data.minutely: []}
-                            timezone={weather.data.timezone}
-                            location_text={weather.location_text}
-                            lat={weather.data.lat}
-                            lon={weather.data.lon}
-                            lat_string={weather.lat_string}
-                            lon_string={weather.lon_string}
-                            handleMapClick={handleMapClick}
-                            googleMapError={googleMapError}
-                        />
-                    </div>
-                    <WeekForecast daily={weather.data.daily} timezone={weather.data.timezone}/>
-                </div>
-            )}
-        </main>
-    )
+          </div>
+          <WeekForecast daily={weather.data.daily} timezone={weather.data.timezone}/>
+        </div>
+      )}
+    </main>
+  )
 }
--- client/src/components/Body/WindVane.tsx
+++ client/src/components/Body/WindVane.tsx
@@ -2,43 +2,50 @@ import './WindVane.sass'
 import {useState, useEffect} from 'react'
 
 interface WindVaneProps {
-    windDirection: number
+  windDirection: number
 }
 
 export default function WindVane({windDirection}: WindVaneProps) {
-    const [adjustedRotation, setAdjustedRotation] = useState(windDirection)
+  const [adjustedRotation, setAdjustedRotation] = useState(windDirection)
 
-    useEffect(() => {
-        setAdjustedRotation(prevRotation => {
-            const delta = windDirection - prevRotation;
-            const shortestRotation = ((delta + 180) % 360) - 180;
-            return prevRotation + shortestRotation;
-        });
+  useEffect(() => {
+    setAdjustedRotation(prevRotation => {
+      const delta = windDirection - prevRotation;
+      const shortestRotation = ((delta + 180) % 360) - 180;
+      return prevRotation + shortestRotation;
+    });
 
-    }, [windDirection]);
+  }, [windDirection]);
 
-    return (
-        <div className={"wind-vane"}>
-            <svg className={"wind-vane-image"} viewBox={"0 0 200 200"} preserveAspectRatio="xMidYMid meet">
-                <circle className={"wind-vane-circle"} cx={"100"} cy={"100"} r={"80"} stroke={"black"}
-                        fill={"transparent"}></circle>
-                <circle className={"wind-vane-circle"} cx={"100"} cy={"100"} r={"60"} stroke={"black"}
-                        fill={"transparent"}></circle>
-                <g className={"wind-vane-arrow"} transform={`rotate(${adjustedRotation}, 100, 100)`}>
-                    <polyline className={"wind-vane-arrow-tail"} points="81 21 100 40 119 21 100 60 81 21" fill={"red"}
-                              stroke={"red"}></polyline>
-                    <line className="wind-vane-arrow-body" x1="100" y1="40" x2="100" y2="170" stroke="red"></line>
-                    <polyline className="wind-vane-arrow-head" points="81 151 100 170 119 151" stroke="red"
-                              fill="red"></polyline>
-                    <circle cx="100" cy="100" r="6" fill="black"></circle>
-                </g>
-                <g className={"wind-direction-text"}>
-                    <text className={"wind-direction"} textAnchor={"middle"} x={100} y={15}>N</text>
-                    <text className={"wind-direction"} textAnchor={"middle"} x={100} y={198}>S</text>
-                    <text className={"wind-direction"} textAnchor={"middle"} x={8} y={105}>W</text>
-                    <text className={"wind-direction"} textAnchor={"middle"} x={187} y={105}>E</text>
-                </g>
-            </svg>
-        </div>
-    )
+  return (
+    <div className={"wind-vane"}>
+      <svg className={"wind-vane-image"} viewBox={"0 0 200 200"} preserveAspectRatio="xMidYMid meet">
+        <circle className={"wind-vane-circle"} cx={"100"} cy={"100"} r={"80"} stroke={"black"}
+                fill={"transparent"}></circle>
+        <circle className={"wind-vane-circle"} cx={"100"} cy={"100"} r={"60"} stroke={"black"}
+                fill={"transparent"}></circle>
+        <g
+          className={"wind-vane-arrow"}
+          style={{
+            transform: `rotate(${adjustedRotation}deg)`,
+            transformOrigin: "100px 100px",
+            transition: "transform 1s ease-in-out"
+          }}
+        >
+          <polyline className={"wind-vane-arrow-tail"} points="81 21 100 40 119 21 100 60 81 21" fill={"red"}
+                    stroke={"red"}></polyline>
+          <line className="wind-vane-arrow-body" x1="100" y1="40" x2="100" y2="170" stroke="red"></line>
+          <polyline className="wind-vane-arrow-head" points="81 151 100 170 119 151" stroke="red"
+                    fill="red"></polyline>
+          <circle cx="100" cy="100" r="6" fill="black"></circle>
+        </g>
+        <g className={"wind-direction-text"}>
+          <text className={"wind-direction"} textAnchor={"middle"} x={100} y={15}>N</text>
+          <text className={"wind-direction"} textAnchor={"middle"} x={100} y={198}>S</text>
+          <text className={"wind-direction"} textAnchor={"middle"} x={8} y={105}>W</text>
+          <text className={"wind-direction"} textAnchor={"middle"} x={187} y={105}>E</text>
+        </g>
+      </svg>
+    </div>
+  )
 }
