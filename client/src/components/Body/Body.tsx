3467c436c0cf9f1ee79219579cc5f782182c93f1
Ken Harmon <kharmon11@gmail.com>
2025-07-21 19:04:07 -0700

    refactor: Added lazy loading to Body component

--- client/src/components/Body/Body.tsx
+++ client/src/components/Body/Body.tsx
@@ -1,11 +1,16 @@
 import './Body.sass'
+import {Suspense, lazy} from 'react'
+
 import type {MapMouseEvent} from "@vis.gl/react-google-maps";
 
 import useWeather from "../../hooks/useWeather.tsx"
 import Spinner from "../common/Spinner.tsx"
 import LocationForm from "./LocationForm.tsx";
-import Current from "./Current.tsx";
-import WeekForecast from "./WeekForecast/WeekForecast.tsx"
+// import Current from "./Current.tsx";
+// import WeekForecast from "./WeekForecast/WeekForecast.tsx"
+
+const Current = lazy(() => import("./Current"))
+const WeekForecast = lazy(() => import("./WeekForecast/WeekForecast.tsx"))
 
 export default function Body() {
   const {
@@ -42,27 +47,27 @@ export default function Body() {
         handleSubmit={handleSubmit}
         handleMyLocation={handleMyLocation}
       />
-      {isLoading && (
-        <Spinner/>
-      )}
+      {isLoading && (<Spinner/>)}
       {weather && (
-        <div className={"weather-output"}>
-          <div className={"current-wrapper"}>
-            <Current
-              current={weather.data.current}
-              minutely={weather.data.minutely ? weather.data.minutely : []}
-              timezone={weather.data.timezone}
-              location_text={weather.location_text}
-              lat={weather.data.lat}
-              lon={weather.data.lon}
-              lat_string={weather.lat_string}
-              lon_string={weather.lon_string}
-              handleMapClick={handleMapClick}
-              googleMapError={googleMapError}
-            />
+        <Suspense>
+          <div className={"weather-output"}>
+            <div className={"current-wrapper"}>
+              <Current
+                current={weather.data.current}
+                minutely={weather.data.minutely ? weather.data.minutely : []}
+                timezone={weather.data.timezone}
+                location_text={weather.location_text}
+                lat={weather.data.lat}
+                lon={weather.data.lon}
+                lat_string={weather.lat_string}
+                lon_string={weather.lon_string}
+                handleMapClick={handleMapClick}
+                googleMapError={googleMapError}
+              />
+            </div>
+            <WeekForecast daily={weather.data.daily} timezone={weather.data.timezone}/>
           </div>
-          <WeekForecast daily={weather.data.daily} timezone={weather.data.timezone}/>
-        </div>
+        </Suspense>
       )}
     </main>
   )
